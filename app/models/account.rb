b80aacbc53255bcaf780fe1bb421b63d504dfcb7
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-08-01 02:37:50 +0300

    Add sha_pass_hash generation

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,14 @@
 
 class Account < ApplicationRecord
   self.table_name = 'account'
+
+  attr_accessor :password
+
+  validates :username, uniqueness: { case_sensitive: true }
+
+  before_create :generate_sha_pass_hash, if: :valid?
+
+  def generate_sha_pass_hash
+    self.sha_pass_hash = Digest::SHA1.hexdigest([username.upcase, password.upcase].join(':'))
+  end
 end
