5c8a137876815d47867b8cbce1140e15232c7226
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-08-15 23:19:15 +0300

    Remove assets config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  config.assets.enabled = false
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
